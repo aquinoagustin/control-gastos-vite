2c348155751a5c2a22ba7786a0d0b9920e79a08e
aquinoagustin <aagustin067@gmail.com>
2023-09-07 08:33:22 -0300

    Creando State Controlpresupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import Header from './components/Header'
 function App() {
   const [presupuesto,setPresupuesto] = useState(0);
+  const [isValidPresupuesto,setIsValidPresupuesto] = useState(false);
   return (
     <div>
-      <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto}/>
+      <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto}
+              isValidPresupuesto={isValidPresupuesto} setIsValidPresupuesto={setIsValidPresupuesto}/>
       <h1>Control-gastos-vite</h1>
     </div>
   )
