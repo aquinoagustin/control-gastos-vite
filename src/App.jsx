2a471c0c7328439c234ecbc779c4bb518ae7b241
Agus2020 <aagustin067@gmail.com>
2023-09-07 21:56:52 -0300

    Agregado state modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import IconoNuevoGasto from '../public/img/nuevo-gasto.svg'
+import Modal from './components/Modal';
 function App() {
   const [presupuesto,setPresupuesto] = useState(0);
   const [isValidPresupuesto,setIsValidPresupuesto] = useState(false);
+  const [modal,setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  }
+
   return (
     <div>
       <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto}
               isValidPresupuesto={isValidPresupuesto} setIsValidPresupuesto={setIsValidPresupuesto}/>
       {isValidPresupuesto && (
       <div className='nuevo-gasto'>
-        <img src={IconoNuevoGasto} alt='Nuevo Gasto'/>
+        <img src={IconoNuevoGasto} alt='Nuevo Gasto' onClick={handleNuevoGasto}/>
       </div>
       )}
+      {modal && <Modal/>}
     </div>
   )
 }
