308953119bbaad5c64890a68b1ad1c74c1cfe6c9
Agus2020 <aagustin067@gmail.com>
2023-09-07 21:57:04 -0300

    Creado componente Modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+export default function Modal (){
+    const [gasto,setGasto] = useState(0);
+    const [cantidad,setCantidad] = useState(0);
+    const [filtro,setFiltro] = useState('');
+    return(
+        <div className="modal">
+            
+        </div>
+    )
+}
