646c8cb7e744385e35d721880961897c1f577119
Agus2020 <aagustin067@gmail.com>
2023-09-11 21:51:39 -0300

    Pasamos el componente gastos

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,10 +1,21 @@
-export default function ControlPresupuesto({presupuesto}){
-    const formatearCantidad = (cantidad) =>{
-        return cantidad.toLocaleString('en-US',{
-            style:'currency',
-            currency:'USD'
-        })
-    }
+import {useState, useEffect } from "react"
+import {formatearCantidad} from '../helpers/index';
+export default function ControlPresupuesto({gastos,presupuesto}){
+
+    const [disponible,setDisponible] = useState();
+    const [gastado,setGastado] = useState();
+   
+   
+
+
+    useEffect(()=>{
+        const totalGastado = gastos.reduce((total,gasto)=>gasto.cantidad + total,0);
+        console.log(totalGastado)
+        setGastado(totalGastado);
+        setDisponible(presupuesto);
+    },[gastos])
+
+
     return(
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
             <div>
@@ -15,10 +26,10 @@ export default function ControlPresupuesto({presupuesto}){
                     <span>Presupuesto:</span>{formatearCantidad(presupuesto)}
                 </p>
                 <p>
-                    <span>Disponible:</span>{formatearCantidad(0)}
+                    <span>Disponible:</span>{formatearCantidad(disponible)}
                 </p>
                 <p>
-                    <span>Gastado:</span>{formatearCantidad(0)}
+                    <span>Gastado:</span>{formatearCantidad(gastado)}
                 </p>
             </div>
         </div>
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,9 +1,28 @@
 import { formatearFecha } from "../helpers";
+import IconoAhorro from '../../public/img/icono_ahorro.svg';
+import IconoCasa from '../../public/img/icono_casa.svg';
+import IconoComida from '../../public/img/icono_comida.svg';
+import IconoGastos from '../../public/img/icono_gastos.svg';
+import IconoOcio from '../../public/img/icono_ocio.svg';
+import IconoSalud from '../../public/img/icono_salud.svg';
+import IconoSuscripciones from '../../public/img/icono_suscripciones.svg';
+
+const diccionarioIconos = {
+  ahorro: IconoAhorro,
+  casa : IconoCasa,
+  comida : IconoComida,
+  gastos : IconoGastos,
+  ocio : IconoOcio,
+  salud : IconoSalud,
+  suscripciones : IconoSuscripciones
+}
+
 export default function Gasto({ gasto }) {
   const { nombre, cantidad, categoria, id,fecha } = gasto;
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
+        <img src={diccionarioIconos[categoria]} alt="Icono"/>
         <div className="descripcion-gasto">
           <p className="categoria">{categoria}</p>
           <p className="nombre-gasto">{nombre}</p>
@@ -12,8 +31,8 @@ export default function Gasto({ gasto }) {
             <span>{formatearFecha(fecha)}</span>
           </p>
         </div>
-        <p className="cantidad-gasto">US${cantidad}</p>
       </div>
+        <p className="cantidad-gasto">US${cantidad}</p>
     </div>
   );
 }
